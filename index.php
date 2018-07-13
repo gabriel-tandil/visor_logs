1970d084b9200c9d5021175ff1935db8543a8ce9
gabrieltandil <galvarez@sinergiasoluciones.com.ar>
2018-07-13 16:42:02 -0300

    una pila de mejoras a los filtros, aun incompleto

--- index.php
+++ index.php
@@ -2,15 +2,50 @@
 <html>
 <title>Visor de Bitácora</title>
 <head>
-<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
-<script type="text/javascript" language="javascript" src="js/jquery.js"></script>
-<script type="text/javascript" language="javascript"
-	src="js/jquery.dataTables.js"></script>
+<link rel="stylesheet" type="text/css"
+	href="DataTables/datatables.min.css" />
+
+<script type="text/javascript" src="DataTables/datatables.min.js"></script>
 <script type="text/javascript" language="javascript">
 			$(document).ready(function() {
+
+			    // Setup - add a text input to each footer cell
+			    $('#log-grid thead tr').clone(true).appendTo( '#log-grid thead' );
+			    $('#log-grid thead tr:eq(1) th').each( function (i) {
+			        var title = $(this).text();
+					if (i==3){
+						$(this).html( '<select  placeholder="Tipo" >'
+												+'<option value="">Todos</option> '
+												+'<option value="general">General</option> '
+												+'<option value="channel">Channel</option> '
+												+'</select>');
+												$( 'select', this ).on( ' change', function () {
+										            
+										            	dataTable
+										                    .column(i)
+										                    .search( $(this).val() )
+										                    .draw();
+										            
+										        } );
+					}else{
+			        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+			 
+			        $( 'input', this ).on( 'keyup change', function () {
+			            if ( dataTable.column(i).search() !== this.value ) {
+			            	dataTable
+			                    .column(i)
+			                    .search( this.value )
+			                    .draw();
+			            }
+			        } );
+					}
+			    } );
+				
 				var dataTable = $('#log-grid').DataTable( {
 					"processing": true,
 					"serverSide": true,
+			        orderCellsTop: true,
+			        fixedHeader: true,
 					"order": [[ 0, "desc" ]], // orden inicial por id descendente
 					"ajax":{
 						url :"log-grid-data.php", // json datasource
@@ -22,25 +57,16 @@
 							
 						}
 					}
+
 				} );
-				$("#log-grid_filter").css("display","none");  // hiding global search box
-				$('.search-input-text').on( 'keyup click', function () {   // for text boxes
-					var i =$(this).attr('data-column');  // getting column index
-					var v =$(this).val();  // getting search input value
-					dataTable.columns(i).search(v).draw();
-				} );
-				$('.search-input-select').on( 'change', function () {   // for select box
-					var i =$(this).attr('data-column');  
-					var v =$(this).val();  
-					dataTable.columns(i).search(v).draw();
-				} );
+				
+
 				
 				
 				
 			} );
 		</script>
 <style>
-
 body {
 	background: #f7f7f7;
 	color: #333;
@@ -69,41 +95,41 @@ body {
 				<th>Operanción</th>
 			</tr>
 		</thead>
-		<thead>
-			<tr>
-				<td><input type="text" data-column="0" class="search-input-text"
-					size="10"></td>
-				<td><input type="text" data-column="1" class="search-input-text"
-					size="10"></td>
-				<td><input type="text" data-column="2" class="search-input-text"
-					size="20"></td>
-				<td><select data-column="3" class="search-input-select">
-						<option value="">Todos</option>
-						<option value="general">General</option>
-						<option value="channel">Channel</option>
-				</select></td>
-				<td><select data-column="4" class="search-input-select">
-						<option value="">Todos</option>
-						<option value="texto-libre">texto-libre</option>
-						<option value="xml-enviado">xml-enviado</option>
-						<option value="xml-recibido">xml-recibido</option>
-				</select></td>
+		<!-- 		<thead> -->
+		<!-- 			<tr> -->
+		<!-- 				<td><input type="text" data-column="0" class="search-input-text" -->
+		<!-- 					size="10"></td> -->
+		<!-- 				<td><input type="text" data-column="1" class="search-input-text" -->
+		<!-- 					size="10"></td> -->
+		<!-- 				<td><input type="text" data-column="2" class="search-input-text" -->
+		<!-- 					size="20"></td> -->
+		<!-- 				<td><select data-column="3" class="search-input-select"> -->
+		<!-- 						<option value="">Todos</option> -->
+		<!-- 						<option value="general">General</option> -->
+		<!-- 						<option value="channel">Channel</option> -->
+		<!-- 				</select></td> -->
+		<!-- 				<td><select data-column="4" class="search-input-select"> -->
+		<!-- 						<option value="">Todos</option> -->
+		<!-- 						<option value="texto-libre">texto-libre</option> -->
+		<!-- 						<option value="xml-enviado">xml-enviado</option> -->
+		<!-- 						<option value="xml-recibido">xml-recibido</option> -->
+		<!-- 				</select></td> -->
 
 
-				<td><input type="text" data-column="5" class="search-input-text"></td>
-				<td><select data-column="6" class="search-input-select">
-							<?php include 'combo-clase.php'; ?>
-					</select></td>
-				<td><select data-column="7" class="search-input-select">
-							<?php include 'combo-metodo.php'; ?>
-				</select></td>
-				<td><select data-column="8" class="search-input-select">
-							<?php include 'combo-operacion.php'; ?>
-				</select></td>
-			</tr>
+		<!-- 				<td><input type="text" data-column="5" class="search-input-text"></td> -->
+		<!-- 				<td><select data-column="6" class="search-input-select"> -->
+		<!-- 						?php include 'combo-clase.php'; ?>-->
+		<!-- 					</select></td> -->
+		<!-- 				<td><select data-column="7" class="search-input-select"> -->
+		<!-- 						?php include 'combo-metodo.php'; ?>-->
+		<!-- 				</select></td> -->
+		<!-- 				<td><select data-column="8" class="search-input-select"> -->
+		<!-- 						?php include 'combo-operacion.php'; ?>-->
+		<!-- 				</select></td> -->
+		<!-- 			</tr> -->
 
 
-		</thead>
+		<!-- 		</thead> -->
 	</table>
 
 </body>
