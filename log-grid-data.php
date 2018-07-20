de1a11b4cfeca3d4e3f5bb50f4076ba00ba42501
gabrieltandil <galvarez@sinergiasoluciones.com.ar>
2018-07-20 13:52:18 -0300

    arreglo menor filtros

--- log-grid-data.php
+++ log-grid-data.php
@@ -12,18 +12,18 @@ $requestData = $_REQUEST;
 $columns = array(
     // datatable column index => database column name
     
-    1 =>  'idLogProcesos',
-    2 =>  'nivel',
-    3 =>  'tablaIdProceso',
-    4 =>  'idProceso',    
-    5 =>  'marcaTemporal',
-    6 =>  'tipoProceso',  
-    7 =>  'tipoDato',     
-    8 =>  'dato',         
-    9 =>  'clase',        
-    10 =>  'metodo',       
-    11 =>  'operancion',
-    12 =>  'estado'   
+    1 => 'idLogProcesos',
+    2 => 'nivel',
+    3 => 'tablaIdProceso',
+    4 => 'idProceso',
+    5 => 'marcaTemporal',
+    6 => 'tipoProceso',
+    7 => 'tipoDato',
+    8 => 'dato',
+    9 => 'clase',
+    10 => 'metodo',
+    11 => 'operancion',
+    12 => 'estado'
 );
 
 $sql = "SELECT 
@@ -40,7 +40,18 @@ $sql = "SELECT
     estado,
     operancion FROM log_procesos WHERE 1 = 1";
 
-if (! empty($requestData['columns'][1]['search']['value']) || ! empty($requestData['columns'][1]['search']['value']) || ! empty($requestData['columns'][2]['search']['value']) || ! empty($requestData['columns'][3]['search']['value']) || ! empty($requestData['columns'][4]['search']['value']) || ! empty($requestData['columns'][5]['search']['value']) || ! empty($requestData['columns'][6]['search']['value']) || ! empty($requestData['columns'][7]['search']['value']) || ! empty($requestData['columns'][8]['search']['value'])) {
+if (! empty($requestData['columns'][1]['search']['value'])
+    || ! empty($requestData['columns'][2]['search']['value'])
+    || ! empty($requestData['columns'][3]['search']['value'])
+    || ! empty($requestData['columns'][4]['search']['value'])
+    || ! empty($requestData['columns'][5]['search']['value'])
+    || ! empty($requestData['columns'][6]['search']['value'])
+    || ! empty($requestData['columns'][7]['search']['value'])
+    || ! empty($requestData['columns'][8]['search']['value'])
+    || ! empty($requestData['columns'][9]['search']['value'])
+    || ! empty($requestData['columns'][10]['search']['value'])
+    || ! empty($requestData['columns'][11]['search']['value'])
+    || ! empty($requestData['columns'][12]['search']['value'])) {
     // getting records as per search parameters
     if (! empty($requestData['columns'][1]['search']['value'])) { // idLogProcesos
         $sql .= " AND idLogProcesos = " . $requestData['columns'][1]['search']['value'];
@@ -51,8 +62,8 @@ if (! empty($requestData['columns'][1]['search']['value']) || ! empty($requestDa
     if (! empty($requestData['columns'][3]['search']['value'])) { // tablaIdProceso
         if ($requestData['columns'][3]['search']['value'] == 'nulo')
             $sql .= " AND ( tablaIdProceso is null ) ";
-            else
-                $sql .= " AND ( tablaIdProceso = '" . $requestData['columns'][3]['search']['value'] . "' ) ";
+        else
+            $sql .= " AND ( tablaIdProceso = '" . $requestData['columns'][3]['search']['value'] . "' ) ";
     }
     if (! empty($requestData['columns'][4]['search']['value'])) { // idProceso
         $sql .= " AND idProceso = " . $requestData['columns'][2]['search']['value'];
@@ -90,14 +101,13 @@ if (! empty($requestData['columns'][1]['search']['value']) || ! empty($requestDa
     if (! empty($requestData['columns'][12]['search']['value'])) { // estado
         $sql .= " AND ( estado = '" . $requestData['columns'][12]['search']['value'] . "' ) ";
     }
-
 }
-$sql .= " ORDER BY " . $columns[$requestData['order'][0]['column']] . "   " . $requestData['order'][0]['dir'] . "   LIMIT " . $requestData['start'] . " ," . $requestData['length'] . "   "; 
+$sql .= " ORDER BY " . $columns[$requestData['order'][0]['column']] . "   " . $requestData['order'][0]['dir'] . "   LIMIT " . $requestData['start'] . " ," . $requestData['length'] . "   ";
 
 $query = mysqli_query($conn, $sql) or die("employee-grid-data.php: get log_procesos");
 
 $data = array();
-$cuenta=0;
+$cuenta = 0;
 while ($row = mysqli_fetch_array($query)) { // preparing an array
     $nestedData = array();
     $nestedData['idLogProcesos'] = $row['idLogProcesos'];
@@ -105,9 +115,9 @@ while ($row = mysqli_fetch_array($query)) { // preparing an array
     $nestedData['marcaTemporal'] = $row['marcaTemporal'];
     $nestedData['tipoProceso'] = $row['tipoProceso'];
     $nestedData['tipoDato'] = $row['tipoDato'];
-    //si se necesita mejorar la velocidad ver esto de aca
-    //quiza se pueda procesar en la base
-    $nestedData['dato'] =htmlentities(mb_convert_encoding($row['dato'], 'UTF-8', 'ASCII'), ENT_SUBSTITUTE, "UTF-8");
+    // si se necesita mejorar la velocidad ver esto de aca
+    // quiza se pueda procesar en la base
+    $nestedData['dato'] = htmlentities(mb_convert_encoding($row['dato'], 'UTF-8', 'ASCII'), ENT_SUBSTITUTE, "UTF-8");
     $nestedData['clase'] = $row['clase'];
     $nestedData['metodo'] = $row['metodo'];
     $nestedData['operancion'] = $row['operancion'];
@@ -115,17 +125,17 @@ while ($row = mysqli_fetch_array($query)) { // preparing an array
     $nestedData['tablaIdProceso'] = $row['tablaIdProceso'];
     $nestedData['estado'] = $row['estado'];
     $data[] = $nestedData;
-    $cuenta++;
+    $cuenta ++;
 }
 
-if ($cuenta==$requestData['length'])
-    $falsaCantRegistros=$requestData['start']+$requestData['length']+1;
-else 
-    $falsaCantRegistros=$requestData['start']+$cuenta;
+if ($cuenta == $requestData['length'])
+    $falsaCantRegistros = $requestData['start'] + $requestData['length'] + 1;
+else
+    $falsaCantRegistros = $requestData['start'] + $cuenta;
 
 $json_data = array(
     "draw" => intval($requestData['draw']), // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw.
-    "recordsTotal" => $falsaCantRegistros, 
+    "recordsTotal" => $falsaCantRegistros,
     "recordsFiltered" => $falsaCantRegistros,
     "data" => $data // total data array
 );
